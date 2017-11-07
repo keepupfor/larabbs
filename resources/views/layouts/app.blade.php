b0d7ed73b47278f4c8412f6ea40cb64d7b91d0c1
keepupfor <namexd@163.com>
2017-11-07 07:50:41 +0000

    集成simditor编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','LaraBBS') - Laravel进阶教程</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
@@ -19,5 +20,6 @@
     @include('layouts._footer')
 </div>
 <script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
