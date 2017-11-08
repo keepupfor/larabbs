744a37f4c1fddb05c38a6aba52ac4f71d6188ee0
keepupfor <namexd@163.com>
2017-11-08 01:44:05 +0000

    Auhor can delete topic

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -46,14 +46,17 @@
                     <div class="operate">
                         <hr>
                         @can('update',$topic)
-                        <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs" role="button">
+                        <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs pull-left" role="button">
                             <i class="glyphicon glyphicon-edit"></i> 编辑
                         </a>
-                        @endcan
-                        @can('destroy',$topic)
-                        <a href="#" class="btn btn-default btn-xs" role="button">
-                            <i class="glyphicon glyphicon-trash"></i> 删除
-                        </a>
+                            <form action="{{ route('topics.destroy', $topic->id) }}" method="post">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button type="submit" class="btn btn-default btn-xs pull-left" style="margin-left: 6px">
+                                    <i class="glyphicon glyphicon-trash"></i>
+                                    删除
+                                </button>
+                            </form>
                         @endcan
                     </div>
 
