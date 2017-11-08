fcfc0f32f42e342f51b04764cece7a4c6420d8fc
keepupfor <namexd@163.com>
2017-11-08 08:34:08 +0000

    user can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,9 +9,18 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+
+    public function created(Reply $reply)
+    {
+        $reply->topic->increment('reply_count',1);
+    }
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decredment('user_reply_count',1);
+    }
     public function creating(Reply $reply)
     {
-        //
+        $reply->content=clean($reply->content,'user_topic_body');
     }
 
     public function updating(Reply $reply)
