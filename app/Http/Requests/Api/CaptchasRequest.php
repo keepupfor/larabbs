f89ae259709e1981a5e8410ce9d621e3dcd3f6b6
vagrant <vagrant@homestead>
2018-04-12 06:00:19 +0000

    add socialiteproviders

--- app/Http/Requests/Api/CaptchasRequest.php
+++ app/Http/Requests/Api/CaptchasRequest.php
@@ -24,7 +24,7 @@ class CaptchasRequest extends FormRequest
     public function rules()
     {
         return [
-            'phone' => 'required|regex:/^1[34578]\d{9}$/|unique:users',
+//            'phone' => 'required|regex:/^1[34578]\d{9}$/|unique:users',
         ];
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use SocialiteProviders\Weixin\WeixinExtendSocialite;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -13,9 +14,12 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
-        ],
+//        'App\Events\Event' => [
+//            'App\Listeners\EventListener',
+//        ],
+    \SocialiteProviders\Manager\SocialiteWasCalled::class=>[
+        'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
+    ],
     ];
 
     /**
