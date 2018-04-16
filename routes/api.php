824e65d8d241a6e867c49207924d4c3f49cf414f
Namexd <namexd@163.com>
2018-04-16 08:15:20 +0000

    话题详情

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ $api->version('v1', [
         $api->get('categories','CategoriesController@index')->name('api.categories.index');
         $api->get('topics','TopicsController@index')->name('api.topics.index');
         $api->get('users/{user}/topics','TopicsController@Userindex')->name('api.topics.userIndex');
+        $api->get('topics/{topic}','TopicsController@show')->name('api.topics.show');
         //需要token验证的接口
         $api->group(['middleware'=>'api.auth'],function ($api){
             //当前登录用户信息
