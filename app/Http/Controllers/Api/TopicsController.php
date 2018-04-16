6f997d5a1db9692e7b63aa65cdb4cbb2303ec2b9
vagrant <vagrant@homestead>
2018-04-16 08:08:35 +0000

    话题列表

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
 use App\Transformers\TopicTransformer;
@@ -10,23 +11,50 @@ use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
-    public function store(TopicRequest $request,Topic $topic)
+    public function index(Request $request, Topic $topic)
     {
-        $topic->fill($request->all());
-        $topic->user_id=$this->user()->id;
-        $topic->save();
-        return $this->response->item($topic,new TopicTransformer())->setStatusCode(201);
+        $query = $topic->query();
+        if ($categoryId = $request->category_id) {
+            $query->where('category_id', $categoryId);
+        }
+
+        switch ($request->order) {
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        $topics = $query->paginate(20);
+        return $this->response->paginator($topics, new TopicTransformer());
     }
 
-    public function update(TopicRequest $request,Topic $topic)
+    public function store(TopicRequest $request, Topic $topic)
     {
-        $this->authorize('update',$topic);
-        $topic->update($request->all());
-        return $this->response->item($topic,new TopicTransformer());
+        $topic->fill($request->all());
+        $topic->user_id = $this->user()->id;
+        $topic->save();
+        return $this->response->item($topic, new TopicTransformer())->setStatusCode(201);
     }
-    public function destroy(Topic $topic){
-        $this->authorize('destroy',$topic);
+
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
+        return $this->response->item($topic, new TopicTransformer());
+    }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
         $topic->delete();
         return $this->response->noContent();
+    }
+
+    public function userIndex(User $user)
+    {
+        $topics = $user->topics()->recent()->paginate(20);
+        return $this->response->paginator($topics, new TopicTransformer());
     }
 }
--- app/TransFormers/TopicTransformer.php
+++ app/TransFormers/TopicTransformer.php
@@ -7,8 +7,11 @@ use League\Fractal\TransformerAbstract;
 
 class TopicTransformer extends TransformerAbstract
 {
+
+    protected $availableIncludes=['user','category'];
     public function transform(Topic $topic)
     {
+
         return [
             'id' => $topic->id,
             'title' => $topic->title,
@@ -23,5 +26,14 @@ class TopicTransformer extends TransformerAbstract
             'created_at' => $topic->created_at->toDateTimeString(),
             'updated_at' => $topic->updated_at->toDateTimeString(),
         ];
+    }
+
+    public function includeUser(Topic $topic)
+    {
+        return $this->item($topic->user,new UserTransformer());
+    }
+    public function includeCategory(Topic $topic)
+    {
+        return $this->item($topic->category,new CategoryTransformer());
     }
 }
